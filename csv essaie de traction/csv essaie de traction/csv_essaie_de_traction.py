b2444f4cbabe99aa5a3a3deaf39313b15e87fb50
adrien vigne <adrien8282@hotmail.fr>
2017-12-12 12:54:12 +0100

    fin tp

---
+++ csv essaie de traction/csv essaie de traction/csv_essaie_de_traction.py
@@ -0,0 +1,33 @@
+from math import *
+from pylab import *
+
+f1=open('essai1.csv','r')
+ligne1=f1.readlines()
+
+X1=[]
+Y1=[]
+k=0
+for ligne in ligne1:
+    ligne=ligne[:-2]
+    if k>0:
+        a=ligne.split(';')
+        X1.append(float(a[0]))
+        Y1.append(float(a[1]))
+    k+=1
+plot(X1,Y1)
+show()
+f1.close()
+
+X=[(X1[k]+X1[k+1]+X1[k+2]+X1[k+3]+X1[k+4]+X1[k+5])/5 for k in range (len(X1)-6)]
+Y=[(Y1[k]+Y1[k+1]+Y1[k+2]+Y1[k+3]+Y1[k+4]+Y1[k+5])/5 for k in range (len(Y1)-6)]
+
+while X[0]==0.0:
+    X.remove(0.0)
+    Y.remove(Y[0])
+
+P=[Y[k]/X[k] for k in range(len(X))]
+P2=[]
+for k in range(len(P)-3):
+    if abs((P[k]+P[k+1]+P[k+2])/3)-P[k]<10**-3:
+        P2.append(P[k])
+print(P2)
